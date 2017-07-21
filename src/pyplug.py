5f9fce63740bb087ea58e490452d90d4c4e4d16b
Arjun Thekkumadathil <3darjun@gmail.com>
2017-07-21 04:28:52 +0000

    Update pyplug.py
    Using better arhitecture for plugin

--- src/pyplug.py
+++ src/pyplug.py
@@ -1,40 +1,106 @@
-import sys
 import os
+import imp
 import yaml
+import plugexception
+
+class PyPlug(object):
+    def __init__(self, **entry):
+        self.__dict__.update(entry)
+
+    def execute(self):
+        loadFile = os.path.join(self.pluginpath, "{0}.py".format(self.name))
+        module = imp.load_source(self.name, loadFile)
+        if self.enable:
+            try:
+                module.main(**self.args)
+            except AttributeError:
+                module.main()
+        else:
+            print('Plugin is disabled')
 
 class PluginManager(object):
-	def __init__(self, config=None):
-		self._plugin_config = yaml.load(file(config))
-		self.setup()
+    """
+    Simple plugin manager works based on configuration files
 
-	def setup(self):
-		try:
-			sys.path.append(os.path.abspath(self.plugin_config['plugin_path']))
-		except KeyError:
-			pass
+    Example:
+            from pyplug import PluginManager
+            pm = PluginManager('config.yaml')
+            pm.runPlugin('plugin_a')
+            or
+            pm.runPlugins()
 
-	def import_plugins(self, plugin_name):
-		m = __import__(plugin_name)
-		return m
+    Attributes:
+            config (path): Configuration file that defines the search path and
+            plugin information in yaml format
+    """
+    def __init__(self, config):
+        self._config = config
+        try:
+            self._plug = yaml.load(file(self.config))
+        except yaml.scanner.ScannerError:
+            msg ='Unable to register plugin configuration, check yaml syntax'
+            raise plugexception.PluginRegisterError(msg)
+        
 
-	def run_plugin(self, plugin_name):
-		args = {}
-		plug = self.import_plugins(plugin_name)
-		try:
-			args = self.plugin_list[plugin_name]['args']
-		except KeyError:
-			pass
-		if self.plugin_list[plugin_name]['enable']:
-			plug.main(**args)
+    def listPlugins(self):
+        '''
+        Lists all the plugins registered from the config file
 
-	def run_all_plugins(self):
-		for plug in self.plugin_list:
-			self.run_plugin(plug)
+        Returns:
+            list: List of plugins names as strin
+        '''
+        try:
+            all_plugins = self._plug['plugins']
+        except KeyError:
+            raise plugexception.PluginError('No plugins registered')
 
-	@property
-	def plugin_config(self):
-	    return self._plugin_config
+        try:
+            return all_plugins.keys()
+        except AttributeError:
+            raise plugexception.PluginError('No plugins registered')
 
-	@property
-	def plugin_list(self):
-	    return self._plugin_config['plugin_list']
+
+    def getPlugin(self, pluginName):
+        '''
+        Gets the config information for a specific plugin
+
+        Returns:
+            dict: plugin parameters in key value pair
+
+        '''
+        try:
+            plugData = self._plug['plugins'][pluginName]
+            plugData['name'] = pluginName
+            plugData['pluginpath'] = self.pluginpath
+        except KeyError:
+            plugexception.PluginNotFoundError('Unable to find plugin')
+        return PyPlug(**plugData)
+
+    def runPlugin(self, pluginName):
+        '''
+        Executes all the specific plugin as per specified argument
+
+        Attributes:
+            pluginName: Name of a specific plugin mentioned in the 
+            configuration file
+        '''
+        plugin = self.getPlugin(pluginName)
+        plugin.execute()
+        return True
+
+    def runPlugins(self):
+        '''
+        Executes all the plugins defined in the configuration file
+        '''
+        for plug in self.listPlugins():
+            self.runPlugin(plug)
+
+        return True
+
+    @property
+    def config(self):
+        return self._config
+
+    @property
+    def pluginpath(self):
+        return self._plug['pluginpath']
